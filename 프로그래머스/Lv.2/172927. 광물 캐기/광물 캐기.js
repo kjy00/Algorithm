20527eec86eef785102db4509f05c382c37d4570
김지윤 <50611288+kjy00@users.noreply.github.com>
2024-01-12 21:28:27 +0900

    [level Lv.2] Title: 광물 캐기, Time: 5.05 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/Lv.2/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/Lv.2/172927. 광물 캐기/광물 캐기.js
@@ -3,14 +3,11 @@ function solution(picks, minerals) {
     let answer = 25 * 50;
     const dfs = (cur, p, picks) => {
         const [dia, iron, stone] = picks;
+        let diaP = 0, ironP = 0, stoneP = 0;
         if (cur >= minerals.length || (!dia && !iron && !stone)) {
             answer = Math.min(answer, p);
             return ;
         }
-        
-        let diaP = 0;
-        let ironP = 0;
-        let stoneP = 0;
         for (let i = 0; i < 5 && cur + i < minerals.length; i++) {
             diaP += 1;
             if (minerals[cur + i] === "diamond") {
